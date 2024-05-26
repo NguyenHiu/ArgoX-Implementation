a14ca724f2a3cace727739615cc69d599186d5f5
Nguyen Trong Hieu <nguyenhieu82132@gmail.com>
2024-05-27 02:53:54 +0700

    update: separate const to constants module

---
+++ constants/const.go
@@ -0,0 +1,15 @@
+package constants
+
+const (
+	BID                 = true
+	ASK                 = false
+	STATUS_LENGTH       = 1
+	NUM_PARTS           = 2
+	ORDER_SIZE          = 127
+	ETH                 = 0
+	GVN                 = 1
+	TRADER              = 0
+	MATCHER             = 1
+	TX_FINALITY_DEPTH   = 1
+	GAVIN_TOKEN_ADDRESS = "29f777ddbd226ff2f06d6d2d9be2b2c39785e0c1"
+)
