0554a4166b68566af09292e806861d5fccea7e2e
Nguyen Trong Hieu <nguyenhieu82132@gmail.com>
2024-05-28 00:25:13 +0700

    update: change demo flow to the communication between matcher & user

--- main.go
+++ main.go
@@ -8,71 +8,55 @@ import (
 	App "github.com/NguyenHiu/lightning-exchange/app"
 	"github.com/NguyenHiu/lightning-exchange/client"
 	"github.com/NguyenHiu/lightning-exchange/constants"
+	"github.com/NguyenHiu/lightning-exchange/matcher"
+	"github.com/NguyenHiu/lightning-exchange/user"
+	utils "github.com/NguyenHiu/lightning-exchange/utils"
 	"github.com/ethereum/go-ethereum/crypto"
-	ethwallet "perun.network/go-perun/backend/ethereum/wallet"
-	"perun.network/go-perun/wire"
-)
-
-const (
-	chainURL = "ws://127.0.0.1:8545"
-	chainID  = 1337
-
-	// Private keys.
-	keyDeployer = "79ea8f62d97bc0591a4224c1725fca6b00de5b2cea286fe2e0bb35c5e76be46e"
-	keyAlice    = "1af2e950272dd403de7a5760d41c6e44d92b6d02797e51810795ff03cc2cda4f"
-	keyBob      = "f63d7d8e930bccd74e93cf5662fde2c28fd8be95edb70c73f1bdd863d07f412e"
 )
 
 func main() {
-	// Deploy contracts.
-	log.Println("Deploying contracts.")
-	adjudicator, assetHolders, appAddress := deployContracts(chainURL, chainID, keyDeployer)
-	app := App.NewVerifyApp(ethwallet.AsWalletAddr(appAddress))
+	matcherIns := matcher.NewMatcher(constants.CHAIN_URL, constants.CHAIN_ID, constants.KEY_DEPLOYER)
+	alice := user.NewUser(constants.KEY_ALICE)
 
-	ethwalletAssetHolders := []ethwallet.Address{}
-	for _, asset := range assetHolders {
-		ethwalletAssetHolders = append(ethwalletAssetHolders, *ethwallet.AsWalletAddr(asset))
-	}
-
-	// Setup clients.
-	log.Println("Setting up clients.")
-	bus := wire.NewLocalBus() // Message bus used for off-chain communication.
-	stakes := []*big.Int{client.EthToWei(big.NewFloat(5)), big.NewInt(5)}
-	alice := SetupClient(bus, chainURL, adjudicator, ethwalletAssetHolders, keyAlice, app, stakes)
-	bob := SetupClient(bus, chainURL, adjudicator, ethwalletAssetHolders, keyBob, app, stakes)
+	bus, adj, ahs, app, stakes := matcherIns.SetupClient(alice.ID)
+	alice.SetupClient(bus, constants.CHAIN_URL, adj, ahs, app, stakes)
 
 	// Print balances before transactions.
-	l := newBalanceLogger(chainURL)
-	l.LogBalances(alice, bob)
+	l := utils.NewBalanceLogger(constants.CHAIN_URL)
+	l.LogBalances(matcherIns.ClientConfigs[alice.ID].AppClient, alice.AppClient)
 
 	// Open app channel and play.
 	log.Println("Opening channel.")
-	appAlice := alice.OpenAppChannel(bob.WireAddress())
-	appBob := bob.AcceptedChannel()
+	ok := matcherIns.OpenAppChannel(alice.ID, alice.AppClient.WireAddress())
+	if !ok {
+		log.Fatalln("OpenAppChannel Failed")
+	}
+	alice.AcceptedChannel()
 
-	newOrder := App.NewOrder(client.EthToWei(big.NewFloat(5)).Int64(), 5, constants.BID, alice.WalletAddressAsEthwallet(), "P")
-	alicePrvKey, err := crypto.HexToECDSA(keyAlice)
+	newOrder := App.NewOrder(client.EthToWei(big.NewFloat(5)).Int64(), 5, constants.BID, alice.AppClient.WalletAddressAsEthwallet(), "P")
+	alicePrvKey, err := crypto.HexToECDSA(constants.KEY_ALICE)
 	if err != nil {
 		panic(err)
 	}
 	newOrder.Sign(*alicePrvKey)
-	appAlice.SendNewOrder(&newOrder)
+	alice.SendNewOrder(&newOrder)
 
-	lastOrder := App.NewOrder(0, 0, constants.BID, alice.WalletAddressAsEthwallet(), "F")
+	lastOrder := App.NewOrder(0, 0, constants.BID, alice.AppClient.WalletAddressAsEthwallet(), "F")
 	lastOrder.Sign(*alicePrvKey)
-	appAlice.SendNewOrder(&lastOrder)
+	alice.SendNewOrder(&lastOrder)
 
 	// Payout.
 	fmt.Println("Settle")
-	appAlice.Settle()
-	appBob.Settle()
+	alice.Settle()
+	matcherIns.Settle(alice.ID)
 
 	// Print balances after transactions.
 	fmt.Println("LogBalances")
-	l.LogBalances(alice, bob)
+	l.LogBalances(matcherIns.ClientConfigs[alice.ID].AppClient, alice.AppClient)
 
 	// Cleanup.
 	fmt.Println("Shutdown")
 	alice.Shutdown()
-	bob.Shutdown()
+	matcherIns.Shutdown(alice.ID)
+
 }
